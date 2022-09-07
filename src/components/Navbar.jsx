2ea3f8b7cdf2570343e7c077a86d47e176170558
Ben-Zard <zardusb@mail.gvsu.edu>
2022-09-06 20:29:22 -0400

    Strip site setup base react and scss

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,47 @@
+// import "../styles/components/Navbar.scss"
+import React from "react";
+import { motion } from "framer-motion";
+import {BrowserRouter, Link} from "react-router-dom";
+function Navbar() {
+
+      return (
+        <motion.div
+          initial={{ opacity: 0, y: -180 }}
+          animate={{ opacity: 1, y: 0 }}
+        //   exit={{ opacity: 1 }}
+          transition={{
+            ease: "easeInOut",
+            duration: 1,
+            delay: 0.6,
+          }}
+          className='header'>
+          <div className='header-inner'>
+            <div className='logo'>Luram</div>
+            <nav className='nav'>
+            <li><Link to='/contact'>1</Link></li>
+              <li>
+                <a href='/strategy'>2</a>
+              </li>
+              <li>
+                <a href='/cases'>3</a>
+              </li>
+              <li>
+                <a href='/about'>4</a>
+              </li>
+              <li>
+                <a href='/why'>5</a>
+              </li>
+            </nav>
+            <div className='contact'>
+              <a href='/contact'>MAIN</a>
+            </div>
+            <div className='hamburger-menu'>
+              <span>laroum</span>
+              <span>lo</span>
+            </div>
+          </div>
+        </motion.div>
+      );
+    };
+    
+  export default Navbar;
---
+++ src/pages/Contact.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
+import Navbar from "../components/Navbar";
+
+function Contact() {
+return (
+<div className='hamburger-menu'>
+    <Navbar/>
+              <span>laroum</span>
+              <span>lo</span>
+              </div>
+);
+}
+ export default Contact;
