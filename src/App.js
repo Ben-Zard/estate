d9e1fdb800f45c19d913ff65e0f56af99501ba4f
Ben-Zard <zardusb@mail.gvsu.edu>
2022-09-07 23:16:06 -0400

    TESTING

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Testimonial from "./components/Testimonial";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Contact from './pages/Contact';
 import About from './pages/About';
-
+import Test from './pages/Test'
 import Animate from './pages/Animate';
 function App() {
   return (
@@ -12,7 +12,7 @@ function App() {
       <Routes>
       <Route path="/e" element={<Animate />} />
       <Route path="/" element={<Contact />} />
-      
+      <Route path="/test" element={<Test />} />
       <Route path="/about" element={<About />} />
       </Routes>
     </BrowserRouter>
---
+++ src/pages/Test.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
+import Testimonial from "../components/Testimonial";
+import ImageSlider from "../components/Testimonial";
+
+function Test() {
+return (
+<div className=''>
+        <Testimonial/>
+        <ImageSlider/>
+            </div>
+);
+}
+ export default Test;
